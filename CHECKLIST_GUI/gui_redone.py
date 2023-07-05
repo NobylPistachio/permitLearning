522059f9af91f3a022d3d479a83bafdd2de542e7
NobylPistachio <NobylPistachio@gmail.com>
2023-07-05 17:24:26 -0400

    added save function, need to fix load function

--- CHECKLIST_GUI/gui_redone.py
+++ CHECKLIST_GUI/gui_redone.py
@@ -1,33 +1,38 @@
 import tkinter as tk
+from tkinter import ttk
 from tkinter import messagebox
 
+#---- TO DO --------------------------------
+# Button to easy open Accela, and PCPAO
+# Add Work description section
+#-------------------------------------------
 #Master window
 class ChecklistGUI():
-    def __init__(self, master, sections:list) -> None:
+    def __init__(self, master, sections:list, checklist_title:str="") -> None:
         self.master = master
         self.sections = sections
+        self.checklist_title = checklist_title
         #create fields
         self.create_fields()
         self.create_checklist()
 
-        self.create_reset_button()
+        self.create_buttons()
+
+    def create_field(self, label_text, grid_row):
+        "This creates a field for notes"
+        label = tk.Label(self.master, text=label_text)
+        label.grid(row=grid_row, column=0, sticky="e")
+        entry = tk.Entry(self.master, width=60)
+        entry.grid(row=grid_row, column=1, padx=5, sticky="w", columnspan=3)
+        return entry
 
 
     def create_fields(self):
-        permit_label = tk.Label(self.master, text="Permit #:")
-        permit_label.grid(row=0, column=0, sticky="e")
-        self.permit_entry = tk.Entry(self.master, width=60)
-        self.permit_entry.grid(row=0, column=1, padx=5, sticky="w", columnspan=3)
+        "This function is for creating the top section of the page, for holding basic identifying information of a permit"
+        self.permit_entry = self.create_field("Permit #:",grid_row=0)
+        self.address_entry = self.create_field("Address:",grid_row=1)
+        self.parcel_entry = self.create_field("Parcel #:",grid_row=2)
 
-        address_label = tk.Label(self.master, text="Address:")
-        address_label.grid(row=1, column=0, sticky="e")
-        self.address_entry = tk.Entry(self.master, width=60)
-        self.address_entry.grid(row=1, column=1, padx=5, sticky="w",columnspan=3)
-
-        parcel_label = tk.Label(self.master, text="Parcel #:")
-        parcel_label.grid(row=2, column=0, sticky="e")
-        self.parcel_entry = tk.Entry(self.master, width=60)
-        self.parcel_entry.grid(row=2, column=1, padx=5, sticky="w",columnspan=3)
 
     def create_checklist(self):
         #get number of checkboxes
@@ -38,22 +43,25 @@ class ChecklistGUI():
 
         #setting up each checkbox, section_label, and note section
         for i, section in enumerate(self.sections):
+
+            #checklist settings
+            populated_row = i+3
+
                 #checkbox
             checkbox_var = tk.BooleanVar()
             checkbox = tk.Checkbutton(self.master, variable=checkbox_var, command=lambda idx=i: self.toggle_checklist_item(idx))
             self.checkboxes.append(checkbox)
-            checkbox.grid(row=i+3, column=0, sticky="e")
+            checkbox.grid(row=populated_row, column=0, sticky="e")
             checkbox_var.set(self.checklist_states[i])
             checkbox.checkbox_var = checkbox_var
                 #section_label
             section_label = tk.Label(self.master, text=section, wraplength=250)
-            section_label.grid(row=i+3, column=1, sticky="w")
+            section_label.grid(row=populated_row, column=1, sticky="w")
                 #note_section
-            note_section = tk.Entry(self.master, width=30)
+            note_section = tk.Entry(self.master, width=50)
                 #to be able to interact with the notes section I need to keep it somewhere, that will be the self.notes_section
             self.notes_section.append(note_section)
-            note_section.grid(row=i+3, column=2, padx=5, sticky="w")
-            
+            note_section.grid(row=populated_row, column=2, padx=5, sticky="w")
 
 
     def toggle_checklist_item(self, index):
@@ -63,8 +71,8 @@ class ChecklistGUI():
         self.checklist_states[index] = not self.checklist_states[index]
         #
 
-    def reset_fields(self):
 
+    def reset_fields(self):
             #checkboxes reset works only once
         confirmation = messagebox.askyesno("Reset Confirmation", "Are you sure you want to reset all fields and checklist?")
         if confirmation:
@@ -79,12 +87,51 @@ class ChecklistGUI():
             # self.update_checklist()
             # self.save_state()  # Save the checklist state
 
-    def create_reset_button(self):
-        reset_button = tk.Button(self.master, text="Reset", command=self.reset_fields)
-        reset_button.grid(row=len(self.sections) + 3, column=0, columnspan=4, pady=10)
+    def create_button(self, button_text:str, button_command, button_column:int):
+        button = tk.Button(self.master, text=button_text, command=button_command)
+        button.grid(row=len(self.sections) + 3, column=button_column, columnspan=1, pady=10, sticky="")
+        return button
+
+    def create_buttons(self):
+        self.reset_button = self.create_button("Reset",self.reset_fields,0)
+        self.save_button = self.create_button("Save",self.save_state,1)
+        self.load_button = self.create_button("Load",self.load_state,2)
+    
+    def save_state(self):
+        self.fields_entry = [self.permit_entry.get(),self.address_entry.get(),self.parcel_entry.get()]
+        state = {
+            'checklist type': self.checklist_title,
+            'fields': [field for field in self.fields_entry],
+            'checklist_states': self.checklist_states,
+            'notes': [entry.get() for entry in self.notes_section]
+        }
+        with open('checklist_state.txt', 'w') as f:
+            for key, value in state.items():
+                f.write(f"{key}: {value}\n")
+    
+    def load_state(self):
+        try:
+            with open('checklist_state.txt', 'r') as f:
+                state = {}
+                for line in f:
+                    key, value = line.strip().split(': ')
+                    if key == 'checklist type':
+                        self.checklist_title = value
+                    elif key == 'fields':
+                        self.fields_entry = value.split(',')
+                    elif key == 'checklist_states':
+                        print([v for v in value.split(',')])
+                        # state[key] = [bool(int(v)) for v in value.split(',')]
+                    elif key == 'notes':
+                        state[key] = value.split(',')
+                self.checklist_states = state.get('checklist_states', [False] * len(self.sections))
+        except FileNotFoundError:
+            self.checklist_states = [False] * len(self.sections)
+
+
 
 #Settings
-title = ""
+title = "BR-RMR Checklist"
 BR_RMR_CHECKLIST:list = ["Check Jurisdiction",
                     "Check for duplicate permits",
                     "Check for Violations",
@@ -100,8 +147,18 @@ checklist = BR_RMR_CHECKLIST
 
 if __name__ == "__main__":
     root = tk.Tk()
-    root.title(title)
-    gui = ChecklistGUI(root,checklist)
+    root.title("Pinellas County Permit Utility")
+    
+    notebook = ttk.Notebook(root)
+    notebook.pack()
+
+    tab1 = tk.Frame(notebook)
+    # tab2 = tk.Frame(notebook)
+
+    notebook.add(tab1, text=title)
+    # notebook.add(tab2, text="Tab 2")
+
+    gui = ChecklistGUI(tab1,checklist,title)
 
     #to close the window
     # root.protocol("WM_DELETE_WINDOW", on_closing)
