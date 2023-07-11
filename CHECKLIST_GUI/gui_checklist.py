ad93f6d0f16d08fb137dbb86fd3bc304bd02c69c
NobylPistachio <NobylPistachio@gmail.com>
2023-07-11 17:19:47 -0400

    minor changes attempting add work description area

--- CHECKLIST_GUI/gui_checklist.py
+++ CHECKLIST_GUI/gui_checklist.py
@@ -13,41 +13,46 @@ from tkinter import messagebox
 # Main class for the GUI
 class ChecklistGUI():
     def __init__(self, master, sections:list, checklist_title:str="") -> None:
+        # Initialize master, sections, and checklist title
         self.master = master
         self.sections = sections
         self.checklist_title = checklist_title
          # Initialize fields, checklist, and buttons
         self.create_fields()
+
+        # self.create_workdescription()
+
         self.create_checklist()
         self.create_buttons()
-     # Function to create a field for notes
     def create_field(self, label_text, grid_row):
-        "This creates a field for notes"
+        """Creates a field for notes"""
         label = tk.Label(self.master, text=label_text)
         label.grid(row=grid_row, column=0, sticky="e")
         entry = tk.Entry(self.master, width=60)
         entry.grid(row=grid_row, column=1, padx=5, sticky="w", columnspan=3)
         return entry
-     # Function to create the top section of the page, for holding basic identifying information of a permit
     def create_fields(self):
-        "This function is for creating the top section of the page, for holding basic identifying information of a permit"
-        self.permit_entry = self.create_field("Permit #:",grid_row=0)
-        self.address_entry = self.create_field("Address:",grid_row=1)
-        self.parcel_entry = self.create_field("Parcel #:",grid_row=2)
-
-        self.fields_entry = [self.permit_entry,self.address_entry,self.parcel_entry]
-     # Function to create the checklist
+        """Creates the top section of the page, for holding basic identifying information of a permit"""
+        self.permit_entry = self.create_field("Permit #:", grid_row=0)
+        self.address_entry = self.create_field("Address:", grid_row=1)
+        self.parcel_entry = self.create_field("Parcel #:", grid_row=2)
+        self.fields_entry = [self.permit_entry, self.address_entry, self.parcel_entry]
+    def create_workdescription(self):
+        label = tk.Label(self.master, text= "Work Description")
+        label.grid(rowspan=2, column=0, columnspan= 1, sticky="e")
+        entry = tk.Entry(self.master, width=60)
+        entry.grid(column = 1)
+        ...
     def create_checklist(self):
-        #get number of checkboxes
-            #if I have a load state I want to override this vvv
+        """Creates the checklist"""
+        # Initialize checklist states, checkboxes, and notes section
         self.checklist_states = [False] * len(self.sections)
         self.checkboxes = []
         self.notes_section = []
          # Setting up each checkbox, section_label, and note section
         for i, section in enumerate(self.sections):
-
-            #checklist settings
-            populated_row = i+3
+            rows,columns = self.master.grid_size()
+            populated_row = i + rows
              # Checkbox settings
             checkbox_var = tk.BooleanVar()
             checkbox = tk.Checkbutton(self.master, variable=checkbox_var, command=lambda idx=i: self.toggle_checklist_item(idx))
@@ -63,15 +68,11 @@ class ChecklistGUI():
                 #to be able to interact with the notes section I need to keep it somewhere, that will be the self.notes_section
             self.notes_section.append(note_section)
             note_section.grid(row=populated_row, column=2, padx=5, sticky="w")
-
-
     def toggle_checklist_item(self, index):
-        "This will keep track of the var or not maybe this is dumb"
-        
-        #get which checkbox it is and reverse it
+        """Toggles the checklist item at the given index"""
         self.checklist_states[index] = not self.checklist_states[index]
-     # Function to reset fields
-    def reset_fields(self,message:bool=True):
+    def reset_fields(self, message:bool=True):
+        """Resets all fields and checklist items"""
         confirmation = True
         if message:
             confirmation = messagebox.askyesno("Reset Confirmation", "Are you sure you want to reset all fields and checklist?")
@@ -79,31 +80,32 @@ class ChecklistGUI():
             for entry in self.notes_section + self.fields_entry:
                 entry.delete(0, tk.END)
             self.checklist_states = [False] * len(self.sections)
-            for num,each in enumerate(self.checkboxes):
+            for num, each in enumerate(self.checkboxes):
                 each.checkbox_var.set(self.checklist_states[num])
-     # Function to create a button
     def create_button(self, button_text:str, button_command, button_column:int):
+        """Creates a button with the given text, command, and column"""
         button = tk.Button(self.master, text=button_text, command=button_command)
-        button.grid(row=len(self.sections) + 3, column=button_column, columnspan=1, pady=10, sticky="")
+        rows,columns = self.master.grid_size()
+        button.grid(row=len(self.sections) + rows, column=button_column, columnspan=1, pady=10, sticky="")
         return button
-     # Function to create buttons
     def create_buttons(self):
-        self.reset_button = self.create_button("Reset",self.reset_fields,0)
-        self.save_button = self.create_button("Save",self.save_state,1)
-        self.load_button = self.create_button("Load",self.load_state,2)
-     # Function to save the state
+        """Creates Reset, Save, and Load buttons"""
+        self.reset_button = self.create_button("Reset", self.reset_fields, 0)
+        self.save_button = self.create_button("Save", self.save_state, 1)
+        self.load_button = self.create_button("Load", self.load_state, 2)
     def save_state(self):
+        """Saves the current state of the checklist"""
         state = {
             'checklist type': self.checklist_title,
             'fields': [field.get() for field in self.fields_entry],
             'checklist_states': self.checklist_states,
             'notes': [entry.get() for entry in self.notes_section]
         }
-        with open('checklist_state.txt', 'w') as f:
+        with open('checklist_state.txt', 'w') as f: #maybe use self.CHECKLIST_STATE_FILE
             for key, value in state.items():
                 f.write(f"{key}: {value}\n")
-     # Function to load the state
     def load_state(self):
+        """Loads the saved state of the checklist"""
         try:
             with open('checklist_state.txt', 'r') as f:
                 state = {}
@@ -111,7 +113,7 @@ class ChecklistGUI():
                     key, value = line.strip().split(': ')
                     if key == 'checklist type':
                         state[key] = value
-                    elif key in ['fields','notes']:
+                    elif key in ['fields', 'notes']:
                         state[key] = [elem.strip("[]'") for elem in value.split(", ")]
                     elif key == 'checklist_states':
                         str_to_bool = lambda x: True if x == "True" else False
@@ -123,13 +125,23 @@ class ChecklistGUI():
                 self.address_entry.insert(0, state["fields"][1])
                 self.parcel_entry.insert(0, state["fields"][2])
                 self.checklist_states = [box for box in state["checklist_states"]]
-                for num,each in enumerate(self.checkboxes):
+                for num, each in enumerate(self.checkboxes):
                     each.checkbox_var.set(self.checklist_states[num])
-                for num,entry in enumerate(self.notes_section):
+                for num, entry in enumerate(self.notes_section):
                     entry.insert(0, state["notes"][num])
         except FileNotFoundError:
             self.checklist_states = [False] * len(self.sections)
-
+        
+        # see if you like this code
+        # try:
+        #     with open(self.CHECKLIST_STATE_FILE, 'r') as f:
+        #         state = self._parse_state_file(f)
+        #         self.reset_fields(False)
+        #         self._populate_state(state)
+        # except FileNotFoundError:
+        #     self.checklist_states = [False] * len(self.sections)
+        # except ValueError as e:
+        #     print(f"Error loading state: {e}")
 
 title = "BR-RMR Checklist"
 BR_RMR_CHECKLIST:list = ["Check Jurisdiction",
@@ -141,7 +153,8 @@ BR_RMR_CHECKLIST:list = ["Check Jurisdiction",
                     "Check submitted documents",
                     "Check NOC requirement",
                     'Enter or verify "Standard Description" and use established naming convention',
-                    "Deem Complete"
+                    "Deem Complete",
+                    "Move to Distribution"
                     ]
 BR_PLM_CHECKLIST:list = ["Check Jurisdiction",
                     "Check for duplicate permits",
