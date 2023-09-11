208f36562b99061e6c8e22ad2bfa8e0046e339c1
NobylPistachio <NobylPistachio@gmail.com>
2023-09-11 16:52:16 -0400

    forgot what I added in these

---
+++ CHECKLIST_GUI/Checklist_Programs/RMR_no_GUI.py
@@ -0,0 +1,13 @@
+
+
+#this is a program that is a simple program that asks questions and assists with RMR_intake
+
+class Permit():
+    def __init__(self,record_number) -> None:
+        self.record_number = record_number
+        self.parcel_number = ...
+
+
+# permit_number = input("What is the Record/Permit Number?\n")
+# Permit(permit_number)
+print(type(...))
